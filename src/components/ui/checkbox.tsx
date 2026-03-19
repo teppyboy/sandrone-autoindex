57ef15b5b3a414feace0f585308bc967f86f9bf4
Nguyễn Thế Hưng <tretrauit@gmail.com>
2026-03-19 19:01:01 +0700

    fix: checkbox not showing correctly

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -9,7 +9,7 @@ function Checkbox({ className, ...props }: CheckboxPrimitive.Root.Props) {
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        'peer inline-flex size-4 shrink-0 items-center justify-center rounded border border-input bg-background text-primary outline-none transition-colors focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:border-primary data-[checked]:bg-primary data-[checked]:text-primary-foreground dark:bg-input/30',
+        'peer inline-flex size-4 shrink-0 items-center justify-center rounded border border-input bg-background text-primary outline-none transition-colors focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:border-primary data-[checked]:bg-primary data-[checked]:text-primary-foreground data-[unchecked]:dark:bg-input/30',
         className,
       )}
       {...props}
